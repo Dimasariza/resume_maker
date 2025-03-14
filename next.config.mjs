6c39bb4beef40dde567b272e1e0370dee813b9f0
Dimas Ariza <roganda.sihombing11@gmail.com>
2025-03-15 00:58:16 +0700

    update page.js next image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        loader: 'custom',
+        loaderFile: './my-loader.ts',
+    },
+};
 
 export default nextConfig;
